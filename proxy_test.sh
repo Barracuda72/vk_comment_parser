658a158e90dc6da45dc74f60a399e95c98278ae6
Your Name <you@example.com>
2020-02-23 12:50:15 +0100

    Our parallel wasn't really parallel

--- proxy_test.sh
+++ proxy_test.sh
@@ -6,10 +6,12 @@ host="https://semograph.org"
 
 temp_file=`mktemp`
 
+jobs=10
+
 rm ${temp_file}
 
 python3 parse_proxy_list.py ${proxy_list} > ${temp_file}
 
-cat ${temp_file} | parallel --tag curl --proxy {} -o /dev/null --silent --head --write-out  '%{http_code}' ${host} --max-time 50 | grep 200 > verified_proxies.txt
+cat ${temp_file} | parallel -j ${jobs} --tag curl --proxy {} -o /dev/null --silent --head --write-out  '%{http_code}' ${host} --max-time 50 | grep 200 > verified_proxies.txt
 
 rm ${temp_file}
