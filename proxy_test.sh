6a870cabb84bf699ead36424b912792d1ad622c0
Barracuda72 <barracuda72@bk.ru>
2020-02-23 14:31:48 +0500

    Script for proxy testing

---
+++ proxy_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+proxy_list = $1
+
+host = "https://semograph.org"
+
+temp_file = `mktemp`
+
+rm ${temp_file}
+
+python3 parse_proxy_list.py ${proxy_list} > ${temp_file}
+
+cat ${temp_file} | parallel --tag curl --proxy {} -o /dev/null --silent --head --write-out  '%{http_code}' ${host} --max-time 50 | grep 200 > verified_proxies.txt
+
+rm ${temp_file}
