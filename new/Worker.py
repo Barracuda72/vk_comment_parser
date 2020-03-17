b1780c6aee92885ac3477cb8ee72392208edff9e
Your Name <you@example.com>
2020-03-17 19:38:27 +0100

    Less verbose output

--- new/Worker.py
+++ new/Worker.py
@@ -48,7 +48,6 @@ class Worker(object):
         # If current user recursion depth is less than configured, collect user data
         if (depth <= config.collector.depth):
             # Collect user data and retrieve user IDs of users whose data should be collected recursively
-            print ("Processing user {}".format(user))
             users = self.vk_collector.collect_user(user)
 
             # If current recursion depth is less than maximum, then put tasks for recursive processing of returned users
