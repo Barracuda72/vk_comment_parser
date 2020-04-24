c6c4cbf4e082bed782ae9d93635f484a88bb3c0e
Barracuda72 <barracuda72@bk.ru>
2020-04-24 19:21:32 +0500

    Basic error checking on the frontend side

--- new/app.py
+++ new/app.py
@@ -17,11 +17,18 @@ def hello_world():
 @app.route('/add_login', methods=['GET', 'POST'])
 def add_login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        data = "{} {}".format(username, password)
-        p.publish_login(data)
-        return "Data added: {}".format(data)
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            if (not username):
+                raise Exception("Username shouldn't be empty!")
+            if (not password):
+                raise Exception("Password shouldn't be empty!")
+            data = "{} {}".format(username, password)
+            p.publish_login(data)
+            return "Data added: {}".format(data)
+        except Exception as e:
+            return "Error processing request: {}".format(e)
     else:
         return "Use POST for credentials!"
 
@@ -30,9 +37,17 @@ def add_work():
     if request.method == 'POST':
         data = request.form['user_ids']
         data = [x.strip() for x in data.split('\n')]
+        errors = []
         for user_id in data:
+            try:
+                user_id = int(user_id)
+            except Exception as e:
+                errors.append("Error processing UID '{}': {}".format(user_id, e))
             message = "{} {}".format(user_id, 0)
             p.publish_work(message)
-        return "Data added: {}".format(data)
+        if (len(errors) == 0):
+            return "Data added: {}".format(data)
+        else:
+            return "There were errors processing your request: <br/>{}".format("<br/>".join(errors))
     else:
         return "Use POST for user IDs!"
