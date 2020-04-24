1f8ee51c9cc4aa57f7b0dc587564262d5717a0c0
Barracuda72 <barracuda72@bk.ru>
2020-04-24 16:22:53 +0500

    Populate login queue & work queue

---
+++ new/populate_logins.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pika
+import sys
+import config
+
+if (len(sys.argv) < 2):
+    print ("Provide file with IDs!")
+    sys.exit(1)
+
+
+filename = sys.argv[1]
+
+vk_credentials = [x.strip() for x in open(filename, "r").readlines()]
+
+credentials = pika.PlainCredentials(config.rabbitmq.username, config.rabbitmq.password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.rabbitmq.host, credentials=credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue=config.rabbitmq.login_queue, durable=True)
+
+for message in vk_credentials:
+    channel.basic_publish(exchange='',
+                      routing_key=config.rabbitmq.login_queue,
+                      body=message.encode('utf-8'),
+                      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
+connection.close()
