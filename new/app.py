ed768540733840a5801d345b4d5aff530f88d298
Barracuda72 <barracuda72@bk.ru>
2020-04-24 18:01:42 +0500

    Small Flask form for data population

---
+++ new/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+#p = Populator()
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', name=None)
+
+@app.route('/add_login', methods=['GET', 'POST'])
+def add_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        data = "{} {}".format(username, password)
+        #p.populate_login(data)
+        return "Data added: {}".format(data)
+    else:
+        return "Use POST for credentials!"
+
+@app.route('/add_work', methods=['GET', 'POST'])
+def add_work():
+    if request.method == 'POST':
+        data = request.form['user_ids']
+        #p.populate_work(data)
+        return "Data added: {}".format(data)
+    else:
+        return "Use POST for user IDs!"
