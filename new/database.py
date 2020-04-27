7448a906f133bc58329e0d932977212d0c856a45
Your Name <you@example.com>
2020-04-27 13:47:12 +0200

    Use Postgres, finally

--- new/database.py
+++ new/database.py
@@ -16,8 +16,11 @@ from entities.Post import Post
 from entities.Photo import Photo
 from entities.Comment import Comment
 
+import config
+
 #engine = create_engine('sqlite:///:memory:', echo=True)
-engine = create_engine('sqlite:////tmp/database.db?check_same_thread=false', echo=False)
+#engine = create_engine('sqlite:////tmp/database.db?check_same_thread=false', echo=False)
+engine = create_engine('postgresql+psycopg2://{}:{}@{}/{}'.format(config.postgres.username, config.postgres.password, config.postgres.host, config.postgres.database), echo=False)
 _session = sessionmaker(bind=engine)
 
 session = _session()
